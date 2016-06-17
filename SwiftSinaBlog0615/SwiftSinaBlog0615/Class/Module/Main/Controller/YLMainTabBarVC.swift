0db12a781b465ad0235dbe5d1691598b601b2e76
Dast1 <myl90@126.com>
2016-06-18 00:41:14 +0800

    设置tabbar图片和字体颜色。

--- SwiftSinaBlog0615/SwiftSinaBlog0615/Class/Module/Main/Controller/YLMainTabBarVC.swift
+++ SwiftSinaBlog0615/SwiftSinaBlog0615/Class/Module/Main/Controller/YLMainTabBarVC.swift
@@ -21,14 +21,18 @@ class YLMainTabBarVC: UITabBarController {
         addChildVC("我", imgName: "tabbar_profile", VC: YLProfileViewController())
     }
     
-    func addChildVC(title: String, imgName: String,  VC: UIViewController){
+    private func addChildVC(title: String, imgName: String,  VC: UIViewController){
         //        不直接传控制器时，如果传控制器名称，需要如下转换
         //        let  jLVC  = NSClassFromString(VCName) as! UIViewController.Type
         //        let lVC = jLVC.init()//控制器初始化，必须是 .init()
         
         VC.title = title
         VC.tabBarItem.image = UIImage(named: imgName)
-        VC.tabBarItem.selectedImage = UIImage(named: imgName + "_highlighted")
+        
+        
+        VC.tabBarItem.selectedImage = UIImage(named: imgName + "_highlighted")?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        VC.tabBarItem.setTitleTextAttributes([NSForegroundColorAttributeName : UIColor.redColor()], forState: UIControlState.Highlighted)
+        
         self.addChildViewController(UINavigationController(rootViewController: VC))
     }
     
